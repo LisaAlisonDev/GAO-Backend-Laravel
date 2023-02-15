11a3c56c8222807659ffa969ba7d0d8cf12e3285
LisaAlisonDev <alisonbarretpro@gmail.com>
2023-02-15 16:16:26 +0400

    Added models for users, bookings and computers table

---
+++ api/app/Models/Booking.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'id',
+        'userId',
+        'computerId',
+        'hour',
+        'date',
+     ];
+}
